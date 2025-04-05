1842f134bd878068a5c7b84cac8fa94aff4d5cad
Kingeden1 <stewjf@yahoo.com>
2025-04-05 20:17:04 +0300

    py19

---
+++ Date,Time and Datetime.py
@@ -0,0 +1,9 @@
+from datetime import date,time,datetime
+today =datetime.now()
+print("Current Date time: ",today)
+date= date.today()
+print("Today's date: ",date)
+print("Date component's: ")
+print("Today's Date: ",date.day)
+print("Today's Month:",date.month)
+print("Today's Year: ",date.year)
